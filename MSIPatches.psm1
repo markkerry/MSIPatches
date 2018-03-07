615c4bf2deecd13d5f76196e5e2ac204c3be3312
Mark Kerry <mark-kerry@outlook.com>
2018-03-07 16:44:50 +0000

    Changes to psm1 and psd1

--- MSIPatches.psm1
+++ MSIPatches.psm1
@@ -1,13 +1,3 @@
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "You need to run this script from an elevated PowerShell prompt!`nPlease start PowerShell as an Administrator..."
-    break
-}
-
-if ($PSVersionTable.PSEdition -eq 'Core') {
-    Write-Warning "This function is not compatible with PowerShell Core as it reiles on the MSI module binaries."
-    break
-}
-
 # Get Public and private functions
 $Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
